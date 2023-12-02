a6bd96a290e5da80190c787596d8e87f34315b95
yasserelmalouani <yasser1998@outlook.it>
2023-12-02 01:48:33 +0100

    chore: create src directory with App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView>
+      <Text>Welcome to NinjaCamp</Text>
+    </SafeAreaView>
+  );
+}
+
+export default App;
