89a9605eb65f61719cfff222cf066756b736807e
yasserelmalouani <yasser1998@outlook.it>
2023-12-02 02:43:06 +0100

    feat: added details screen

--- src/ui/navigation/root/root.navigator.tsx
+++ src/ui/navigation/root/root.navigator.tsx
@@ -1,13 +1,17 @@
 import React, {useRef} from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {HomeScreen} from '../../screens/home/home.screen';
+import {DetailsScreen} from '../../screens/details/details.screen';
 
 export const RootNavigator = () => {
   const Stack = useRef(createNativeStackNavigator()).current;
 
   return (
-    <Stack.Navigator screenOptions={{headerShown: true}}>
+    <Stack.Navigator
+      screenOptions={{headerShown: true}}
+      initialRouteName={'Home'}>
       <Stack.Screen name={'Home'} component={HomeScreen} />
+      <Stack.Screen name={'Details'} component={DetailsScreen} />
     </Stack.Navigator>
   );
 };
---
+++ src/ui/screens/details/details.screen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+type Props = {};
+
+export const DetailsScreen: React.FC<Props> = () => {
+  return (
+    <SafeAreaView>
+      <Text>Welcome to Details Screen</Text>
+    </SafeAreaView>
+  );
+};
