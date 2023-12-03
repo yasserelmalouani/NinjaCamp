876558344a5633a59679e3e3ba618c8e7cc975d5
yasserelmalouani <yasser1998@outlook.it>
2023-12-03 23:01:05 +0100

    feat: added navigation to details

--- src/ui/screens/home/home.screen.tsx
+++ src/ui/screens/home/home.screen.tsx
@@ -1,12 +1,19 @@
-import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import React, {useCallback} from 'react';
+import {Button, SafeAreaView, Text} from 'react-native';
+import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 
-type Props = {};
+type Props = {
+  navigation: NativeStackNavigationProp<any>;
+};
 
-export const HomeScreen: React.FC<Props> = () => {
+export const HomeScreen: React.FC<Props> = ({navigation}) => {
+  const goToDetails = useCallback(() => {
+    navigation.navigate('Details');
+  }, [navigation]);
   return (
     <SafeAreaView>
       <Text>Welcome to NinjaCamp</Text>
+      <Button title="Go to Details" onPress={goToDetails} />
     </SafeAreaView>
   );
 };
